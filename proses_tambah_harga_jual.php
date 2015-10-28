661217edec971535989dab3694f2afd417947420
49u5edy <edyagusc@gmail.com>
2015-10-28 19:37:18 +0700

    Test Comit Use Sublime

--- proses_tambah_harga_jual.php
+++ proses_tambah_harga_jual.php
@@ -3,10 +3,10 @@ ob_start();
 session_start();
 include "koneksi.php";
 
-// $id_hj=$_POST["id_hj"];
-$id_user=$_SESSION["id_user"];
-$kode_produk=$_POST["kode_produk"];
-$harga_jual=$_POST["harga_jual"];
+// $id_hj    = $_POST["id_hj"];
+$id_user     = $_SESSION["id_user"];
+$kode_produk = $_POST["kode_produk"];
+$harga_jual  = $_POST["harga_jual"];
 
 $query="insert into harga_jual(id_user,kode_produk,harga_jual) values('$id_user','$kode_produk','$harga_jual')";
 
